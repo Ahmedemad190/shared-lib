bea2194bb197a2e57ebd17724c6df4ce42251b1d
Ahmedemad190 <130619786+Ahmedemad190@users.noreply.github.com>
2024-12-26 00:38:52 +0200

    Update deployToOpenShift.groovy

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
 //OpenShiftCredentialsID can be credentials of service account token or KubeConfig file 
-def call(String OpenShiftCredentialsID, String openshiftClusterurl, String openshiftProject, String imageName) {
+def call(String OpenShiftCredentialsID, String openshiftClusterurl, String openshiftProject, String imageName, String BUILD_NUMBER) {
     
     // login to OpenShift Cluster via cluster url & service account token
     withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
             sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
-                       sh "oc project ahmedemad"
-                        sh "oc new-app ${DOCKER_IMAGE_NAME}:${BUILD_NUMBER} --name=lab-app-ivolve-last"
-                        sh "oc scale deployment/lab-app-ivolve-last --replicas=3"
+            sh "oc apply -f deployment.yml"
+            sh "oc apply -f service.yml"
+            sh "oc expose svc ivolve-project-service"
     }
 
 }
