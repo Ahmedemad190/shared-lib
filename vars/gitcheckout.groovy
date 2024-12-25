0cb88a79f6752a948d5b2917b729fa602e26388e
Ahmedemad190 <130619786+Ahmedemad190@users.noreply.github.com>
2024-12-26 01:01:00 +0200

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,5 +1,5 @@
 #!usr/bin/env groovy
 def call() {
 	echo "checking Git Repo..."
-	git branch: 'main', credentialsId: 'GitHub', url: 'https://github.com/Ahmedemad190/MultiCloudDevOpsProject.git'	
+	git branch: 'main',  url: 'https://github.com/Ahmedemad190/MultiCloudDevOpsProject.git'	
 }
